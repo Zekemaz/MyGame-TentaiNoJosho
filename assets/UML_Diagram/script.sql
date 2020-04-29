f07a2e3f451657e7a67124136fcff84c4ea9c901
Gabe <desseresusinigabriel@gmail.com>
2020-04-29 15:41:21 +0200

    update MCD / MLD

--- assets/UML_Diagram/script.sql
+++ assets/UML_Diagram/script.sql
@@ -7,7 +7,7 @@
 # Table: user
 #------------------------------------------------------------
 
-CREATE TABLE user(
+CREATE TABLE `user`(
         ID_user       Int  Auto_increment  NOT NULL ,
         lastname      Varchar (50) NOT NULL ,
         firstname     Varchar (50) NOT NULL ,
@@ -15,6 +15,7 @@ CREATE TABLE user(
         email         Varchar (50) NOT NULL ,
         pseudo        Varchar (20) NOT NULL ,
         password      Varchar (36) NOT NULL ,
+        last_activity TimeStamp NOT NULL
 	,CONSTRAINT user_PK PRIMARY KEY (ID_user)
 )ENGINE=InnoDB;
 
@@ -23,10 +24,10 @@ CREATE TABLE user(
 # Table: skin
 #------------------------------------------------------------
 
-CREATE TABLE skin(
+CREATE TABLE `skin`(
         ID_skin    Int  Auto_increment  NOT NULL ,
         skin_name  Varchar (50) NOT NULL ,
-        skin_image Text NOT NULL
+        skin_image Varchar (50) NOT NULL
 	,CONSTRAINT skin_PK PRIMARY KEY (ID_skin)
 )ENGINE=InnoDB;
 
@@ -35,9 +36,11 @@ CREATE TABLE skin(
 # Table: character
 #------------------------------------------------------------
 
-CREATE TABLE character(
+CREATE TABLE `character`(
         ID_character      Int  Auto_increment  NOT NULL ,
         pseudo            Varchar (50) NOT NULL ,
+        class             Varchar (10) NOT NULL ,
+        level             Int NOT NULL ,
         experience        Int NOT NULL ,
         money             Int NOT NULL ,
         strength          Int NOT NULL ,
@@ -59,14 +62,25 @@ CREATE TABLE character(
 # Table: message
 #------------------------------------------------------------
 
-CREATE TABLE message(
+CREATE TABLE `message`(
         ID_message         Int  Auto_increment  NOT NULL ,
         message_content    Varchar (255) NOT NULL ,
-        message_created_at Datetime NOT NULL ,
-        message_author     Varchar (20) NOT NULL ,
+        message_created_at TimeStamp NOT NULL ,
         ID_user            Int NOT NULL
 	,CONSTRAINT message_PK PRIMARY KEY (ID_message)
 
 	,CONSTRAINT message_user_FK FOREIGN KEY (ID_user) REFERENCES user(ID_user)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: experience_table
+#------------------------------------------------------------
+
+CREATE TABLE `experience_table`(
+        level               Int NOT NULL ,
+        required_exp        Int NOT NULL ,
+        experience_per_kill Int NOT NULL
+	,CONSTRAINT experience_table_PK PRIMARY KEY (level)
 )ENGINE=InnoDB;
 
